457b45bec6e718827b36bf02b7a578b08c2bcef4
Nik Butenko <nik@butenko.me>
2014-07-29 23:46:23 +1000

    Configurable app port

--- app.js
+++ app.js
@@ -6,19 +6,20 @@ var _ = require('underscore');
 
 
 var environment = process.argv[2] || 'dev';
+var port = process.env.PORT || 3000;
 var assetsDir = (environment === 'dev') ? './src' : './build';
 
 
 var file = new nodeStatic.Server(assetsDir);
 var indexContent = _.template(fs.readFileSync(path.join(__dirname, assetsDir, 'index.html'), 'utf-8'))({env: environment});
 require('http').createServer(function (request, response) {
-	request.addListener('end',function () {
+	request.addListener('end', function () {
 		console.log(['Serving: ', request.url].join(''));
 		if (request.url === '/' || request.url === '/index.html') {
 			return response.end(indexContent);
 		}
 		return file.serve(request, response);
 	}).resume();
-}).listen(3000);
+}).listen(port);
 
-console.info(['Running on http://localhost:3000, environment: ', environment, ', assets: ', assetsDir].join(''));
+console.info(['Running on http://localhost:', port, ', environment: ', environment, ', assets: ', assetsDir].join(''));
